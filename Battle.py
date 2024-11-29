8633f500c583f2ad7a9667cbee61636d6b5304c7
Abdykadyr Turduev <abdykadyr.turduev@alatoo.edu.kg>
2024-11-29 13:09:10 +0600

    third commit.py

--- Battle.py
+++ Battle.py
@@ -2,4 +2,18 @@ import random
 import os
 def clear_screen():
   os.system('cls' if os.name == 'nt' else 'clear')
+  def display_board(board, reveal=False):
+    print("  A B C D E F G")
+    for row in range(7):
+        display_row = []
+        for col in range(7):
+            cell = board[row][col]
+            if not reveal:
+                if cell == "S":  
+                    display_row.append(" ")
+                else:
+                    display_row.append(cell)
+            else:
+                display_row.append(cell)
+        print(f"{row+1} {' '.join(display_row)}")
   
