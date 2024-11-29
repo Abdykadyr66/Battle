ae0a86b6af4241d9a443ad1a12c6ac9ba411a7e6
Abdykadyr Turduev <abdykadyr.turduev@alatoo.edu.kg>
2024-11-29 13:10:59 +0600

    sixth commit.py

--- Battle.py
+++ Battle.py
@@ -24,5 +24,13 @@ def is_valid_position(board, ship):
             return False
         if board[r][c] == "S" or board[r][c] == "X":
             return False
+ for (r, c) in ship:
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                nr, nc = r + i, c + j
+                if 0 <= nr < 7 and 0 <= nc < 7 and board[nr][nc] == "S":
+                    return False
+
+    return True
 
   
